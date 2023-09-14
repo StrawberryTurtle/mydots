b69c0f9de02cbb35ba11ba81104fa2cb92e8584e
StrawberryTurtle <kateramsier@gmail.com>
2023-09-14 12:40:55 -0700

    testing

--- config/bspwm/polybar/polybar.sh
+++ config/bspwm/polybar/polybar.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-## Copyright (C) 2020-2023 Aditya Shakya <adi1090x@gmail.com>
-
 ## Files and Directories
 SFILE="$HOME/.config/bspwm/polbar/system.ini"
 RFILE="$HOME/.config/bspwm/polybar/.system"
